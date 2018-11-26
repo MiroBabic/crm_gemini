9be42a195f5e8db7a04839f2c72973d801dd1fdf
MiroBabic <babic003@gmail.com>
2018-11-26 23:04:59 +0000

    fix in path

--- cronjobs/delayed_job.sh
+++ cronjobs/delayed_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source $HOME/.profile
 #cd /home/rails/uvo && RAILS_ENV=production bin/delayed_job restart
-cd /home/miro/uvo && RAILS_ENV=production bin/delayed_job restart
+cd /home/miro/crm_gemini && RAILS_ENV=production bin/delayed_job restart
