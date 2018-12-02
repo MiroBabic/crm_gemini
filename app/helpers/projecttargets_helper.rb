93cc412109fcde72243e0fd95127851890e191c9
MiroBabic <babic003@gmail.com>
2018-12-02 23:06:20 +0000

    Added count for projectargets

--- app/helpers/projecttargets_helper.rb
+++ app/helpers/projecttargets_helper.rb
@@ -1,7 +1,11 @@
 module ProjecttargetsHelper
 		def count_total_subject_for_projecttarget(id)
-		if (id.present? && (id.to_i > 0))
-			total = Subject.where(:subjtype_id=>id).count
+
+		@result  = ActiveRecord::Base.connection.select_all(ActiveRecord::Base.send(:sanitize_sql_array, [query = "select * from subjects s, json_array_elements_text(s.project_targets) as obj where obj::integer = ?;", id]))
+			
+
+		if (@result.present?)
+			total = @result.rows.count
 		else
 			total = 0
 		end
