4d1b350272e9dbd80a4d5e6aced296874704ae8d
MiroBabic <babic003@gmail.com>
2019-07-31 22:32:39 +0100

    update import controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -6,14 +6,14 @@ class ImportsController < ApplicationController
 
   def importsubjects
   	 
-     #begin
+     begin
       
      @new= Subject.import(params[:file])
       
         redirect_to imports_path, notice: "#{@new[:success]} subjektov bolo uspesne vlozenych a #{@new[:failed]} nepreslo. Spolu bolo zaznamov:  #{@new[:total]}"
-      #rescue
-       # redirect_to imports_path, alert: "Import nepresiel. Skontroluj subor a jeho strukturu."
-      #end
+      rescue=>error
+        redirect_to imports_path, alert: error.message
+      end
 
   end
 
